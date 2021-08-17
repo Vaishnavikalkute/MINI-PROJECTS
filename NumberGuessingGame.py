4ebdc42637d0e56be7531d8c905d8841f725bfe7
Vaishnav Kalkute <53871902+Vaishnavikalkute@users.noreply.github.com>
2021-08-17 23:59:43 +0530

    Add files via upload

---
+++ NumberGuessingGame.py
@@ -0,0 +1,21 @@
+import random
+num=random.randint(1,10)
+
+Player_Name=input("Hey there!, What's your Name?")
+num_of_try=0
+print("So "+Player_Name+ " guess the number between 1-10 ;)")
+
+while num_of_try<5:
+    guess=int(input())
+    num_of_try+=1
+    if guess<num:
+        print("Its to low")
+    elif guess>num:
+        print("It's To high")
+    elif guess==num:
+        break
+if guess==num:
+    print("Uh You Read My Mind in "+str(num_of_try)+" guess")
+else:
+    print("hehe It's Never easy to read my mind but still to the number is"+str(num))
+    
