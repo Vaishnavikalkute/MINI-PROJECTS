2e0941b3a2088c3f0cf525a30fd33ad67a1c4ea4
Vaishnavi kalkute <vaishnavi.kalkute@punctuations.in>
2024-06-02 00:15:20 +0530

    processing and listening to command

--- custom_redis.py
+++ custom_redis.py
@@ -16,10 +16,32 @@ class redisServers():
         self.server_socket.listen(5)
 
     def start(self):
-        # handle the connection start
-        # accept method will accept the connection request and return the new socket and client addresss
-        client_server,addres=self.server_socket.accept()
+        # purpose of while true is to keep our server active 
+        # accept will ne on stand by until new connection request is coming
+        while True:
+            # handle the connection start
+            # accept method will accept the connection request and return the new socket and client addresss
+            client_server,addres=self.server_socket.accept()
+            # make the new socket run on new thread 
+            threading.Thread(target=self.handle_client,args=(client_server,)).start()
+
+    def handle_client(self,client_server):
+        # with will make to close the connection after the process 
+        with client_server:
+            # run the loop until client_server is connected
+            while True:
+                # condiiton to check the data from client
+                data=client_server.recv(1024).decode()
+                if not data():
+                    break
+                # condition to prcess data and send i back to client
+            
+
+    def process_command(self,command):
         pass
+
+
+        
 if __name__ == "__main__":
     server=redisServers()
     server.start()
