61eeb907daac5d94e8a838e28c7e178b80773ab6
Vaishnavi kalkute <vaishnavi.kalkute@punctuations.in>
2024-06-01 22:21:22 +0530

    starting with basic server connection

--- custom_redis.py
+++ custom_redis.py
@@ -12,10 +12,13 @@ class redisServers():
         self.server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         # make connection to the specified port
         self.server_socket.bind(self.host,self.port)
+        # only 5 pending connection are accepted to stay in queue
         self.server_socket.listen(5)
 
     def start(self):
         # handle the connection start
+        # accept method will accept the connection request and return the new socket and client addresss
+        client_server,addres=self.server_socket.accept()
         pass
 if __name__ == "__main__":
     server=redisServers()
