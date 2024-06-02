690f887f5a05e002043e68b6c1da5e71fc12329f
Vaishnavi kalkute <vaishnavi.kalkute@punctuations.in>
2024-06-02 17:22:32 +0530

    final relize

--- custom_redis.py
+++ custom_redis.py
@@ -1,47 +1,59 @@
 import socket
 import threading
 
-
-class redisServers():
-    def __init__(self,host='127.0.0.1',port=5000):
-        self.host=host
-        self.port=port
-        # setup in machine data storage
-        self.data={}
-        # setting up tcp connection request
-        self.server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        # make connection to the specified port
-        self.server_socket.bind(self.host,self.port)
-        # only 5 pending connection are accepted to stay in queue
+class RedisServer:
+    def __init__(self, host='127.0.0.1', port=5000):
+        self.host = host
+        self.port = port
+        # In-memory data storage
+        self.data = {}
+        # Setting up TCP connection request
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Bind the socket to the specified host and port
+        self.server_socket.bind((self.host, self.port))
+        # Allow the server to accept connections, with a queue of up to 5 pending connections
         self.server_socket.listen(5)
+        print(f"Listening on {self.host}:{self.port}")
 
     def start(self):
-        # purpose of while true is to keep our server active 
-        # accept will ne on stand by until new connection request is coming
+        # Keep the server active to accept connections
         while True:
-            # handle the connection start
-            # accept method will accept the connection request and return the new socket and client addresss
-            client_server,addres=self.server_socket.accept()
-            # make the new socket run on new thread 
-            threading.Thread(target=self.handle_client,args=(client_server,)).start()
+            # Accept a new connection request
+            client_socket, address = self.server_socket.accept()
+            print(f"Connection from {address}")
+            # Handle the new connection in a separate thread
+            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
 
-    def handle_client(self,client_server):
-        # with will make to close the connection after the process 
-        with client_server:
-            # run the loop until client_server is connected
+    def handle_client(self, client_socket):
+        # Automatically close the connection when done
+        with client_socket:
             while True:
-                # condiiton to check the data from client
-                data=client_server.recv(1024).decode()
-                if not data():
+                # Receive data from the client
+                data = client_socket.recv(1024).decode()
+                if not data:
                     break
-                # condition to prcess data and send i back to client
-            
+                # Process the command and send a response back to the client
+                response = self.process_command(data.strip())
+                client_socket.sendall(response.encode())
 
-    def process_command(self,command):
-        pass
+    def process_command(self, command):
+        parts = command.split()
+        cmd = parts[0].upper()
 
+        if cmd == "SET" and len(parts) == 3:
+            self.data[parts[1]] = parts[2]
+            return "OK"
+        elif cmd == "GET" and len(parts) == 2:
+            return self.data.get(parts[1], "None")
+        elif cmd == "DEL" and len(parts) == 2:
+            if parts[1] in self.data:
+                del self.data[parts[1]]
+                return "OK"
+            else:
+                return "Key does not exist"
+        else:
+            return "ERR unknown command"
 
-        
 if __name__ == "__main__":
-    server=redisServers()
+    server = RedisServer()
     server.start()
